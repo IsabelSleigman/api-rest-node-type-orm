e9dbb9bbf553170e17d675914323c470c62e3eb7
isabelsleigman <sleimusic@gmail.com>
2022-01-28 09:52:59 -0300

    instalado uuid para gerar id automatico, ajustado para salvar dados no banco e ajustado controller

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,16 +1,31 @@
+import { getRepository } from 'typeorm';
+import { Usuario } from '../entities/Usuario';
 interface IUsuario{
+    id: string,
     nome: string,
-    email: string
+    email?: string
 }
 
 class CreateUserService{
 
-    execute({nome, email}:IUsuario){
-        const data = [];
+   async execute({id, nome, email}:IUsuario){
 
-        data.push({nome,email})
+        const usuario = await getRepository(Usuario)
+            .createQueryBuilder()
+            .insert()
+            .into(Usuario)
+            .values([
+                {
+                    id: id,
+                    nome: nome,
+                    email: email
+                }
+            ])
+            .execute();
 
-        return data;
+        console.log(usuario);
+
+        return usuario;
     }
 }
 
